88f0ed6f554c15ed5ffbe9aaa0867ec2bbc72188
k.l.lambda <k.l.lambda@gmail.com>
2022-09-10 10:06:48 +0800

    index.vue: added result list.

--- app/index.vue
+++ app/index.vue
@@ -1,9 +1,19 @@
 <template>
-	<div>
+	<div class="home">
 		<header>
 			<input v-model="description" type="text" />
 			<button @click="paintByText">dream</button>
 		</header>
+		<main>
+			<section v-for="(result, i) of results" :key="i">
+				<div v-if="result.images">
+					<img v-for="(img, ii) of result.images" :key="ii"
+						:src="img"
+					/>
+				</div>
+				<hr />
+			</section>
+		</main>
 	</div>
 </template>
 
@@ -15,6 +25,7 @@
 		data () {
 			return {
 				description: null,
+				results: [],
 			};
 		},
 
@@ -24,6 +35,8 @@
 				const response = await fetch(`/paint-by-text?prompt=${encodeURIComponent(this.description)}&multi=1`);
 				const result = await response.json();
 				console.log("result:", result);
+
+				this.results.push(result);
 			},
 		},
 	};
@@ -34,11 +47,27 @@
 	body
 	{
 		white-space: nowrap;
+		margin: 0;
+		height: 100vh;
+	}
+
+	.home
+	{
+		display: flex;
+		flex-direction: column;
+		height: 100%;
 	}
 
 	header
 	{
-		height: 2em;
 		margin: 20px 0;
+		padding: 0 1em;
+		flex: 0 0 2em;
+	}
+
+	main
+	{
+		overflow-y: auto;
+		flex: 1 1;
 	}
 </style>
