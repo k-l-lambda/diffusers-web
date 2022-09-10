c3a007adde4213972c6e2c0aff6fd66e70e43464
k.l.lambda <k.l.lambda@gmail.com>
2022-09-10 10:18:39 +0800

    index.vue: enhanced result content.

--- app/index.vue
+++ app/index.vue
@@ -1,11 +1,12 @@
 <template>
 	<div class="home">
 		<header>
-			<input v-model="description" type="text" />
-			<button @click="paintByText">dream</button>
+			<input class="description" v-model="description" type="text" />
+			<button @click="paintByText">&#x1f4ad;</button>
 		</header>
 		<main>
 			<section v-for="(result, i) of results" :key="i">
+				<div><em>{{result.prompt}}</em><span v-if="result.loading">...</span></div>
 				<div v-if="result.images">
 					<img v-for="(img, ii) of result.images" :key="ii"
 						:src="img"
@@ -32,11 +33,18 @@
 
 		methods: {
 			async paintByText () {
+				const item = {
+					prompt: this.description,
+					loading: true,
+				};
+				this.results.push(item);
+
 				const response = await fetch(`/paint-by-text?prompt=${encodeURIComponent(this.description)}&multi=1`);
 				const result = await response.json();
-				console.log("result:", result);
+				//console.log("result:", result);
 
-				this.results.push(result);
+				Object.assign(item, result);
+				item.loading = false;
 			},
 		},
 	};
@@ -63,11 +71,27 @@
 		margin: 20px 0;
 		padding: 0 1em;
 		flex: 0 0 2em;
+		display: flex;
+	}
+
+	header .description
+	{
+		flex: 1 1;
+	}
+
+	header button
+	{
+		flex: 0 0 4em;
 	}
 
 	main
 	{
 		overflow-y: auto;
 		flex: 1 1;
+	}
+
+	main section
+	{
+		padding: 1em;
 	}
 </style>
