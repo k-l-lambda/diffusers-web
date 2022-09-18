d7bc71a9b42281386c1d4e787f05baf00a009de7
k-l-lambda <k.l.lambda@gmail.com>
2022-09-18 11:18:05 +0800

    pipeline_stable_diffusion.py: inpaint added.

--- pipeline_stable_diffusion.py
+++ pipeline_stable_diffusion.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Union
 import base64
 import struct
 import PIL.Image
+from tqdm.auto import tqdm
 
 import numpy as np
 import torch
@@ -26,7 +27,22 @@ def preprocess (image):
 	image = np.array(image).astype(np.float32) / 255.0
 	image = image[None].transpose(0, 3, 1, 2)[:, :3, :, :]
 	image = torch.from_numpy(image)
+	if image.shape[1] == 1:
+		image = image.repeat((1, 3, 1, 1))
 	return 2.0 * image - 1.0
+
+
+def preprocess_mask(mask):
+	mask = mask.convert("L")
+	w, h = mask.size
+	w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
+	mask = mask.resize((w // 8, h // 8), resample=PIL.Image.NEAREST)
+	mask = np.array(mask).astype(np.float32) / 255.0
+	mask = np.tile(mask, (4, 1, 1))
+	mask = mask[None].transpose(0, 1, 2, 3)  # what does this step do?
+	mask = 1 - mask  # repaint white, keep black
+	mask = torch.from_numpy(mask)
+	return mask
 
 
 def encodeFloat32 (tensor):
@@ -125,7 +141,6 @@ class StableDiffusionPipeline (DiffusionPipeline):
 		generator: Optional[torch.Generator] = None,
 		latents: Optional[torch.FloatTensor] = None,
 		output_type: Optional[str] = "pil",
-		return_dict: bool = True,
 		**kwargs,
 	):
 		r"""
@@ -160,9 +175,6 @@ class StableDiffusionPipeline (DiffusionPipeline):
 			output_type (`str`, *optional*, defaults to `"pil"`):
 				The output format of the generate image. Choose between
 				[PIL](https://pillow.readthedocs.io/en/stable/): `PIL.Image.Image` or `nd.array`.
-			return_dict (`bool`, *optional*, defaults to `True`):
-				Whether or not to return a [`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] instead of a
-				plain tuple.
 
 		Returns:
 			[`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] or `tuple`:
@@ -300,9 +312,6 @@ class StableDiffusionPipeline (DiffusionPipeline):
 		if output_type == "pil":
 			image = self.numpy_to_pil(image)
 
-		if not return_dict:
-			return (image, has_nsfw_concept)
-
 		return dict(images=image, latents=latent_codes, nsfw_content_detected=has_nsfw_concept)
 
 
@@ -334,7 +343,6 @@ class StableDiffusionPipeline (DiffusionPipeline):
 		eta: Optional[float] = 0.0,
 		generator: Optional[torch.Generator] = None,
 		output_type: Optional[str] = "pil",
-		return_dict: bool = True,
 	):
 		r"""
 		Function invoked when calling the pipeline for generation.
@@ -369,9 +377,6 @@ class StableDiffusionPipeline (DiffusionPipeline):
 			output_type (`str`, *optional*, defaults to `"pil"`):
 				The output format of the generate image. Choose between
 				[PIL](https://pillow.readthedocs.io/en/stable/): `PIL.Image.Image` or `nd.array`.
-			return_dict (`bool`, *optional*, defaults to `True`):
-				Whether or not to return a [`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] instead of a
-				plain tuple.
 
 		Returns:
 			[`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] or `tuple`:
@@ -510,7 +515,189 @@ class StableDiffusionPipeline (DiffusionPipeline):
 		if output_type == "pil":
 			image = self.numpy_to_pil(image)
 
-		if not return_dict:
-			return (image, has_nsfw_concept)
-
 		return dict(images=image, latents=latent_codes, nsfw_content_detected=has_nsfw_concept)
+
+
+	@torch.no_grad()
+	def inpaint(
+		self,
+		prompt: Union[str, List[str]],
+		init_image: Union[torch.FloatTensor, PIL.Image.Image],
+		mask_image: Union[torch.FloatTensor, PIL.Image.Image],
+		strength: float = 0.8,
+		num_inference_steps: Optional[int] = 50,
+		guidance_scale: Optional[float] = 7.5,
+		eta: Optional[float] = 0.0,
+		generator: Optional[torch.Generator] = None,
+		output_type: Optional[str] = "pil",
+	):
+		r"""
+		Function invoked when calling the pipeline for generation.
+
+		Args:
+			prompt (`str` or `List[str]`):
+				The prompt or prompts to guide the image generation.
+			init_image (`torch.FloatTensor` or `PIL.Image.Image`):
+				`Image`, or tensor representing an image batch, that will be used as the starting point for the
+				process. This is the image whose masked region will be inpainted.
+			mask_image (`torch.FloatTensor` or `PIL.Image.Image`):
+				`Image`, or tensor representing an image batch, to mask `init_image`. White pixels in the mask will be
+				replaced by noise and therefore repainted, while black pixels will be preserved. If `mask_image` is a
+				PIL image, it will be converted to a single channel (luminance) before use. If it's a tensor, it should
+				contain one color channel (L) instead of 3, so the expected shape would be `(B, H, W, 1)`.
+			strength (`float`, *optional*, defaults to 0.8):
+				Conceptually, indicates how much to inpaint the masked area. Must be between 0 and 1. When `strength`
+				is 1, the denoising process will be run on the masked area for the full number of iterations specified
+				in `num_inference_steps`. `init_image` will be used as a reference for the masked area, adding more
+				noise to that region the larger the `strength`. If `strength` is 0, no inpainting will occur.
+			num_inference_steps (`int`, *optional*, defaults to 50):
+				The reference number of denoising steps. More denoising steps usually lead to a higher quality image at
+				the expense of slower inference. This parameter will be modulated by `strength`, as explained above.
+			guidance_scale (`float`, *optional*, defaults to 7.5):
+				Guidance scale as defined in [Classifier-Free Diffusion Guidance](https://arxiv.org/abs/2207.12598).
+				`guidance_scale` is defined as `w` of equation 2. of [Imagen
+				Paper](https://arxiv.org/pdf/2205.11487.pdf). Guidance scale is enabled by setting `guidance_scale >
+				1`. Higher guidance scale encourages to generate images that are closely linked to the text `prompt`,
+				usually at the expense of lower image quality.
+			eta (`float`, *optional*, defaults to 0.0):
+				Corresponds to parameter eta (η) in the DDIM paper: https://arxiv.org/abs/2010.02502. Only applies to
+				[`schedulers.DDIMScheduler`], will be ignored for others.
+			generator (`torch.Generator`, *optional*):
+				A [torch generator](https://pytorch.org/docs/stable/generated/torch.Generator.html) to make generation
+				deterministic.
+			output_type (`str`, *optional*, defaults to `"pil"`):
+				The output format of the generate image. Choose between
+				[PIL](https://pillow.readthedocs.io/en/stable/): `PIL.Image.Image` or `np.array`.
+			return_dict (`bool`, *optional*, defaults to `True`):
+				Whether or not to return a [`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] instead of a
+				plain tuple.
+
+		Returns:
+			[`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] or `tuple`:
+			[`~pipelines.stable_diffusion.StableDiffusionPipelineOutput`] if `return_dict` is True, otherwise a `tuple.
+			When returning a tuple, the first element is a list with the generated images, and the second element is a
+			list of `bool`s denoting whether the corresponding generated image likely represents "not-safe-for-work"
+			(nsfw) content, according to the `safety_checker`.
+		"""
+		if isinstance(prompt, str):
+			batch_size = 1
+		elif isinstance(prompt, list):
+			batch_size = len(prompt)
+		else:
+			raise ValueError(f"`prompt` has to be of type `str` or `list` but is {type(prompt)}")
+
+		if strength < 0 or strength > 1:
+			raise ValueError(f"The value of strength should in [0.0, 1.0] but is {strength}")
+
+		# set timesteps
+		self.scheduler.set_timesteps(num_inference_steps)
+
+		# preprocess image
+		if not isinstance(init_image, torch.FloatTensor):
+			init_image = preprocess(init_image)
+		init_image = init_image.to(self.device)
+
+		# encode the init image into latents and scale the latents
+		init_latent_dist = self.vae.encode(init_image).latent_dist
+		init_latents = init_latent_dist.sample(generator=generator)
+
+		init_latents = 0.18215 * init_latents
+
+		# Expand init_latents for batch_size
+		init_latents = torch.cat([init_latents] * batch_size)
+		init_latents_orig = init_latents
+
+		# preprocess mask
+		if not isinstance(mask_image, torch.FloatTensor):
+			mask_image = preprocess_mask(mask_image)
+		mask_image = mask_image.to(self.device)
+		mask = torch.cat([mask_image] * batch_size)
+
+		# check sizes
+		if not mask.shape == init_latents.shape:
+			raise ValueError("The mask and init_image should be the same size!")
+
+		# get the original timestep using init_timestep
+		offset = self.scheduler.config.get("steps_offset", 0)
+		init_timestep = int(num_inference_steps * strength) + offset
+		init_timestep = min(init_timestep, num_inference_steps)
+		timesteps = self.scheduler.timesteps[-init_timestep]
+		timesteps = torch.tensor([timesteps] * batch_size, dtype=torch.long, device=self.device)
+
+		# add noise to latents using the timesteps
+		noise = torch.randn(init_latents.shape, generator=generator, device=self.device)
+		init_latents = self.scheduler.add_noise(init_latents, noise, timesteps)
+
+		# get prompt text embeddings
+		text_input = self.tokenizer(
+			prompt,
+			padding="max_length",
+			max_length=self.tokenizer.model_max_length,
+			truncation=True,
+			return_tensors="pt",
+		)
+		text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+
+		# here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
+		# of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
+		# corresponds to doing no classifier free guidance.
+		do_classifier_free_guidance = guidance_scale > 1.0
+		# get unconditional embeddings for classifier free guidance
+		if do_classifier_free_guidance:
+			max_length = text_input.input_ids.shape[-1]
+			uncond_input = self.tokenizer(
+				[""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
+			)
+			uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+
+			# For classifier free guidance, we need to do two forward passes.
+			# Here we concatenate the unconditional and text embeddings into a single batch
+			# to avoid doing two forward passes
+			text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+
+		# prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
+		# eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
+		# eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
+		# and should be between [0, 1]
+		accepts_eta = "eta" in set(inspect.signature(self.scheduler.step).parameters.keys())
+		extra_step_kwargs = {}
+		if accepts_eta:
+			extra_step_kwargs["eta"] = eta
+
+		latents = init_latents
+		t_start = max(num_inference_steps - init_timestep + offset, 0)
+		for i, t in tqdm(enumerate(self.scheduler.timesteps[t_start:])):
+			# expand the latents if we are doing classifier free guidance
+			latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+
+			# predict the noise residual
+			noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+
+			# perform guidance
+			if do_classifier_free_guidance:
+				noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+				noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+			# compute the previous noisy sample x_t -> x_t-1
+			latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
+
+			# masking
+			init_latents_proper = self.scheduler.add_noise(init_latents_orig, noise, t)
+			latents = (init_latents_proper * mask) + (latents * (1 - mask))
+
+		# scale and decode the image latents with vae
+		latents = 1 / 0.18215 * latents
+		image = self.vae.decode(latents).sample
+
+		image = (image / 2 + 0.5).clamp(0, 1)
+		image = image.cpu().permute(0, 2, 3, 1).numpy()
+
+		# run safety checker
+		#safety_cheker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(self.device)
+		#image, has_nsfw_concept = self.safety_checker(images=image, clip_input=safety_cheker_input.pixel_values)
+		has_nsfw_concept = None
+
+		if output_type == "pil":
+			image = self.numpy_to_pil(image)
+
+		return dict(images=image, nsfw_content_detected=has_nsfw_concept)
