2797e113d0f5888ff3bf93e66ad5746542d2eed7
k.l.lambda <k.l.lambda@gmail.com>
2023-05-03 14:36:42 +0800

    main.py: fixed syntax error.

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def randomSentenceV2 ():
 
 def main (argv):
 	global pipe, senGen2, senGen, rand_generator
-	pipe = StableDiffusionPipeline.from_pretrained(DIFFUSER_MODEL_PATH, use_auth_token=HF_TOKEN, torch_dtype=torch.float16 if FLOAT16 else torch_dtype=torch.float32)
+	pipe = StableDiffusionPipeline.from_pretrained(DIFFUSER_MODEL_PATH, use_auth_token=HF_TOKEN, torch_dtype=torch.float16 if FLOAT16 else torch.float32)
 
 	device = torch.device(f'{DEVICE}:{TEXT_DEVICE_INDEX}') if DEVICE else None
 	senGen = SentenceGenerator(templates_path='corpus/templates.txt', reserved_path='corpus/reserved.txt', device=device)
